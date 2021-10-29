ed4932aa918dd84209e65785dee09f1acdc430ac
dinsvraka <dsvraka1@etf.unsa.ba>
2021-10-29 12:42:11 +0200

    Prva verzija

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int velicina = 0;
+
+    public boolean dodajArtikl(Artikl artikl) {
+        if (velicina >= 50) return false;
+        artikli[velicina] = artikl;
+        velicina = velicina + 1;
+        return true;
+    }
+    public Artikl[] getArtikli() { return artikli; }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < velicina; i++) {
+            if(kod.equals(artikli[i].getKod())) {
+                Artikl vrati = artikli[i];
+                artikli[i] = null;
+                System.arraycopy(artikli, i+1, artikli, i, velicina-i);
+                velicina = velicina - i;
+                return vrati;
+            }
+        }
+        return null;
+    }
+    public int dajUkupnuCijenuArtikala() {
+        int ukupno = 0;
+        for(int i = 0; i < velicina; i++) {
+            ukupno = ukupno + artikli[i].getCijena();
+        }
+        return ukupno;
+    }
+    public int getVel() {return velicina; }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private int velicina = 0;
+
+    public void dodajArtikl(Artikl artikl) {
+        artikli[velicina] = artikl;
+        velicina = velicina + 1;
+    }
+
+    public Artikl[] getArtikli() { return artikli; }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i = 0; i < velicina; i++) {
+            int pomocni = Integer.parseInt(artikli[i].getKod());
+            if ( pomocni == Integer.parseInt(kod)) {
+                Artikl vrati = artikli[i];
+                System.arraycopy(artikli, i+1, artikli, i, velicina - 1);
+                velicina = velicina - 1;
+                return vrati;
+            }
+        }
+        return null;
+    }
+    public int getVel() { return velicina; }
 }
